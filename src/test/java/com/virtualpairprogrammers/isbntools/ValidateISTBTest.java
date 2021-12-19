7d89311156e7aceba209d485eaec48a273823bc0
pceja <But1g3112,.->
2021-12-19 16:14:32 -0600

    Upgrade ValidateISTBTest tests to JUnit5

--- src/test/java/com/virtualpairprogrammers/isbntools/ValidateISTBTest.java
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISTBTest.java
@@ -1,38 +1,40 @@
 package com.virtualpairprogrammers.isbntools;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidateISTBTest {
 
     @Test
-    public void validTenDigitsISBN(){
+    public void validTenDigitsISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean isValidISBN = validator.checkISBN("0140449116");
 
-        assertTrue("First value",isValidISBN);
-        isValidISBN= validator.checkISBN("0140177396");
-        assertTrue("Second value",isValidISBN);
+        assertTrue(isValidISBN, "First value");
+        isValidISBN = validator.checkISBN("0140177396");
+        assertTrue(isValidISBN, "Second value");
     }
 
     @Test
-    public void validISBNThirteenDigit(){
+    public void validISBNThirteenDigit() {
         ValidateISBN validator = new ValidateISBN();
         boolean isValidISBN = validator.checkISBN("9781853260087");
         assertTrue(isValidISBN);
-        isValidISBN= validator.checkISBN("9781853267338");
+        isValidISBN = validator.checkISBN("9781853267338");
         assertTrue(isValidISBN);
     }
 
     @Test
-    public void validISBNEndingWithX(){
+    public void validISBNEndingWithX() {
         ValidateISBN validator = new ValidateISBN();
         boolean isValidISBN = validator.checkISBN("012000030X");
         assertTrue(isValidISBN);
     }
 
     @Test
-    public void invalidLessThanTenDigitsISBN(){
+    public void invalidLessThanTenDigitsISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean isValidISBN = validator.checkISBN("0140449117");
 
@@ -40,22 +42,22 @@ public class ValidateISTBTest {
     }
 
     @Test
-    public void invalidThirthteenDigitsISBN(){
+    public void invalidThirthteenDigitsISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean isValidISBN = validator.checkISBN("9781853260088");
 
         assertFalse(isValidISBN);
     }
 
-    @Test(expected = NumberFormatException.class)
-    public void checkInvalidNineDigitsISBN(){
+    @Test
+    public void checkInvalidNineDigitsISBN() {
         ValidateISBN validator = new ValidateISBN();
-        validator.checkISBN("014044911");
+        Assertions.assertThrows(NumberFormatException.class, () -> validator.checkISBN("014044911"));
     }
 
-    @Test(expected = NumberFormatException.class)
-    public void checkInvalidLettersISBN(){
+    @Test
+    public void checkInvalidLettersISBN() {
         ValidateISBN validator = new ValidateISBN();
-        validator.checkISBN("helloWorld");
+        Assertions.assertThrows(NumberFormatException.class, () -> validator.checkISBN("helloWorld"));
     }
 }
